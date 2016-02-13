303e9f96917533b2459623afcf6d13ae93345d09
Nishant Mor <nishantmor27@gmail.com>
2016-02-13 20:29:57 +0530

    v-alpha

--- README.md
+++ README.md
@@ -1,8 +1,10 @@
 # YouTube-Playlist-Time
 Calculates the total time of a YouTube Playlist from its url .
 
-Usage: 
-##python playlist.py
+##Usage: 
+python playlist.py
 Then enter the url of playlist.
 
-  
+##Dependencies:
+1. BeautifulSoup
+2. requests
--- playlist.py
+++ playlist.py
@@ -1,21 +1,16 @@
 import requests
 from bs4 import BeautifulSoup
 
-
-
 def play(url):
 
-		#url = 'https://www.youtube.com/playlist?list=PLEsfXFp6DpzQFqfCur9CJ4QnKQTVXUsRy'
-		#url ='https://www.youtube.com/playlist?list=PLLyDdzyN8G5ol5GmeHoPXxvzilAO3v2ii'
 		source_code = requests.get(url)   # source code of page
 		plain_text = source_code.text
 		soup = BeautifulSoup(plain_text , "html.parser")
 		soup.encode('UTF-8')
 		h , m , s = 0 , 0 , 0;
 		count = 0
-		for link in soup.findAll('div' , {'class' : 'timestamp'} ):      # a = find all links of the titles 
-			#href = link.get('href')     # pulling links of this class
-			         # here string is for <href = "something"...> Titlesndjsnd </href>   then it is Titlesndjsnd 
+		for link in soup.findAll('div' , {'class' : 'timestamp'} ):     
+ 
 			count+=1
 			time = link.string.split(':')
 			if len(time)==3:
@@ -26,26 +21,22 @@ def play(url):
 				m+= int(time[0])
 				s+= int(time[1])
 
-		#print(h , m , s)
-			
+		ts = h*60*60 + m*60 + s
+		m, s = divmod(ts, 60)
+		h, m = divmod(m, 60)
+		d, h = divmod(h ,24)
 
-		ns = s//60
-		nm = (ns+m)//60
-
-		h+=nm
-		d=h//24
-		nh = nm%24 
-		nm = (m+ns)%60
-		ns = s%60
-
-		#print(time , nh , nm , ns)
 		print("Total Videos : " , count)
-		print(d , "Days" , nh , "Hours" , nm , "Minutes" , ns , "Seconds")
-# 			print(time , nh , nm , ns)
-
-
+		print(d , "Days" , h , "Hours" , m , "Minutes" , s , "Seconds")
 
 
 if __name__ == "__main__":
-	url = input()
+	
+	'''
+	url example :  'https://www.youtube.com/playlist?list=PLEsfXFp6DpzQFqfCur9CJ4QnKQTVXUsRy'
+	'''
+
+	url = input("Enter Url:   ")
 	play(url)
+
+
